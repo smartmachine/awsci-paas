3d92f7cbe23f4f5592b48862a0b1c9cbae229768
Conrad Christensen <conrad.christensen@gmail.com>
2019-08-27 23:50:04 +0200

    awsci root project

--- bin/awsci.js
+++ bin/awsci.js
@@ -3,4 +3,9 @@ const cdk = require('@aws-cdk/core');
 const { AwsciStack } = require('../lib/awsci-stack');
 
 const app = new cdk.App();
-new AwsciStack(app, 'AwsciStack');
+new AwsciStack(app, 'AwsciStack', {
+    env: {
+        account: "865054731072",
+        region: "eu-west-1"
+    }
+});
