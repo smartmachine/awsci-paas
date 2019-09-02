d6f6abe9a6b98399921cf00524c625addfea0fa1
Conrad Christensen <conrad.christensen@gmail.com>
2019-09-02 16:09:34 +0200

    adding cfn customresource for final cognito provisioning

--- bin/awsci.js
+++ bin/awsci.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 const cdk = require('@aws-cdk/core');
 const { AwsciStack } = require('../lib/awsci-stack');
+const { CustomResourceStack } = require('../lib/custom-resource');
 
 const app = new cdk.App();
+
+new CustomResourceStack(app, 'CustomResourceStack', {
+    env: {
+        account: "865054731072",
+        region: "eu-west-1"
+    }
+});
+
 new AwsciStack(app, 'AwsciStack', {
     env: {
         account: "865054731072",
