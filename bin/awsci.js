7fdffe02545b90b6100230fadcd57c52b642d002
Conrad Christensen <conrad.christensen@gmail.com>
2019-09-12 23:14:30 +0200

    using zap for structured json logging.

--- bin/awsci.js
+++ bin/awsci.js
@@ -16,8 +16,8 @@ const props = {
     authDomain:      'auth.awsci.io',
     baseCertArn:     'arn:aws:acm:us-east-1:865054731072:certificate/1c19f070-0b8f-4001-9561-6cb32b740170',
     wildcardCertArn: 'arn:aws:acm:us-east-1:865054731072:certificate/aa30e21f-cb3d-4eb1-bc53-f8710e9a3072',
-    callbackUrl:     'https://awsci.io/login',
-    logoutUrl:       'https://awsci.io/logout'
+    callbackUrl:     'http://localhost:3000/login',
+    logoutUrl:       'http://localhost:3000/logout'
 };
 
 new AwsciStack(app, 'AwsciStack', props);
